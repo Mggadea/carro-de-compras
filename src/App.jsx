04ae3856658796ac22e64769c62bd2a1c4d43b6e
Mggadea <matiasgaraygadea@outlook.com>
2022-02-01 01:11:30 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import Catalogo from "./components/screens/Catalogo";
 import Carrito from "./components/screens/Carrito";
 import EstadoCompra from "./components/screens/EstadoCompra";
 import Navbar from "./components/atoms/Navbar";
 
-function App() {
+import { loadProducts } from "./redux/slices/productosSlice";
+import { loadUser } from "./redux/slices/usuarioSlice";
+
+import products from "./data/products.json";
+import profile from "./data/profile.json";
+
+const App = () => {
+  const dispatch = useDispatch();
+  dispatch(loadProducts(products.productos));
+  dispatch(loadUser(profile.profile));
+
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Catalogo />} />
         <Route path="/carrito" element={<Carrito />} />
@@ -16,6 +27,6 @@ function App() {
       </Routes>
     </>
   );
-}
+};
 
 export default App;
