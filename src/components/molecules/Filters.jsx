1d4cfbc698259807b2e2f2dac08ef22c5b6d1154
Mggadea <matiasgaraygadea@outlook.com>
2022-02-02 15:30:28 -0300

    paginación

--- src/components/molecules/Filters.jsx
+++ src/components/molecules/Filters.jsx
@@ -4,6 +4,7 @@ import SortBy from "../atoms/SortBy";
 
 const Filters = ({filtro, setFiltro, ordenar, setOrdenar }) => {
   return <div className="flex flex-row justify-between w-full items-center px-3 ">
+    
   <SearchProduct filtro={filtro} setFiltro={setFiltro} />
   <SortBy ordenar={ordenar} setOrdenar={setOrdenar} />
 </div>;
